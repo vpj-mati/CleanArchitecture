e2f2cf4de1f4a1e0ceb6d8501230cf2b92c87222
matias.arguello@cgtscorp.es <matias.arguello@cgtscorp.es>
2023-04-02 03:25:05 -0300

    Actualizo name space

--- src/Clients/NSwagClients.g.cs
+++ src/Clients/NSwagClients.g.cs
@@ -13,7 +13,7 @@
 #pragma warning disable 3016 // Disable "CS3016 Arrays as attribute arguments is not CLS-compliant"
 #pragma warning disable 8603 // Disable "CS8603 Possible null reference return"
 
-namespace ProcesoAutonomo.ServiceA.NSwagClients
+namespace ProcesoAutonomo.ServiceA.HttpClients.NSwagClients
 {
     using System = global::System;
 
---
+++ src/Clients/NSwagClientsSettings.cs
@@ -0,0 +1,5 @@
+﻿namespace ProcesoAutonomo.ServiceA.HttpClients;
+public class NSwagClientsSettings
+{
+    public string UriString { get; set; } = "https://localhost:5001";
+}
